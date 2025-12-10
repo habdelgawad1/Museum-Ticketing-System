933a5e52fde1d10085ee4f46bbd2f4509461ca6f
habdelgawad1 <ha2400611@tkh.edu.eg>
2025-12-10 15:16:47 +0200

    Fixed Bcrypt Call

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,6 +1,6 @@
 const {db} = require('../config/db');
 const logger = require('../utils/logger.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const createTour = (req, res) => {
     guideID = req.body.guideID
